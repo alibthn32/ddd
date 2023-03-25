9a1592b73df0f519db89b0cb9d7a3788e113a9ff
alibthn32 <batuhanzt3@gmail.com>
2023-03-26 01:14:56 +0300

    ddd

---
+++ Hw2/src/RandomMazeSolver.java
@@ -0,0 +1,45 @@
+
+public class RandomMazeSolver {
+
+	RandomMazeSolver() {
+		
+    }
+
+    static SolverResult solve(World world, int experimentCount) {
+        SolverResult solverResult = new SolverResult();
+
+        int trialFactor = 100;
+        int maximumTrialCount = trialFactor * world.getArea();
+
+        for (int i = 0; i < experimentCount; i++) {
+            if (i % 256 == 0) {
+                System.out.print(".");
+            }
+
+            executeExperiment(world, maximumTrialCount);
+
+            if (world.isRobotDead()) {
+                solverResult.incrementDead();
+            } else {
+                if (world.isRobotAtEnd()) {
+                    solverResult.incrementSuccess();
+                } else {
+                    solverResult.incrementFailure();
+                }
+            }
+        }
+
+        System.out.println("\n");
+
+        return solverResult;
+    }
+
+    static void executeExperiment(World world, int maximumTrialCount) {
+        for (int i = 0; i < maximumTrialCount; i++) {
+            world.moveRobotRandomly();
+            
+        }
+        return;
+    }
+
+}
